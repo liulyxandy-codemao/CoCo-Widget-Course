b4fd940f9da388867ed84e92fdd45dba95d3cfaa
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-04-19 11:10:19 +0800

    移除多余的version字段

--- 2.2/example.js
+++ 2.2/example.js
@@ -2,7 +2,6 @@ const types = {
     type: 'EXAMPLE_WIDGET',
     icon: 'https://coco.codemao.cn/favicon.ico',
     title: '示例控件',
-    version: '1.0.0',
     isInvisibleWidget: true,
     isGlobalWidget: true,
     properties: [],
